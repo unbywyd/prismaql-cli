db5480c561c49fda3b7fc3eddb37f30150fcf2b4
unbywyd <unbywyd@gmail.com>
2025-03-08 21:46:38 +0200

    +

--- output/bin.d.ts
+++
@@ -1,3 +0,0 @@
-declare const loadQueryRenderManager: (options?: Record<string, boolean | string | number>) => Promise<(sourceCommand: string) => Promise<void>>;
-export default loadQueryRenderManager;
-//# sourceMappingURL=bin.d.ts.map
